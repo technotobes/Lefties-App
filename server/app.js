e06d1a168a9540ed24b4f0b8c9a6e3d3c9ef3b65
Toby <tobywas@gmail.com>
2022-03-18 10:16:33 -0400

    heroku

--- server/app.js
+++ server/app.js
@@ -16,6 +16,8 @@ app.use(bodyParser.json())
 app.use(cors())
 app.use(express.json())
 
+const PORT = process.env.PORT || 8080 
+
 // Serve Frontend
 
 if(process.env.NODE_ENV === 'production') {
@@ -204,6 +206,6 @@ app.post('/listing/update/:id', (req, res) => {
     })
 })
 
-app.listen(8080, () => {
+app.listen(PORT, () => {
     console.log('Server is running')
 })
